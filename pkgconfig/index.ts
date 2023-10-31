a21c7622da3bb75942b1d66dbd04e017af6b2a84
Victor Queiroz <victorcqueirozg@gmail.com>
2023-10-31 19:53:26 -0300

    create project to create CMake interface libraries from .pc file

---
+++ pkgconfig/index.ts
@@ -0,0 +1,3 @@
+export { default as PkgConfigFileParser } from "./PkgConfigFileParser";
+export { default as resolveVariables } from "./resolveVariables";
+export { default as mapVariablePositions } from "./mapVariablePositions";
