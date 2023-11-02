c2872a7b4233ff27f301a02130cb74717a4e8ce1
Victor Queiroz <victorcqueirozg@gmail.com>
2023-11-02 13:04:53 -0300

    add core module

---
+++ core/index.ts
@@ -0,0 +1,7 @@
+export { default as PkgConfigFileParser } from "./PkgConfigFileParser";
+export { default as resolveVariables } from "./resolveVariables";
+export { default as Tokenizer, IToken, TokenType } from "./Tokenizer";
+export { default as InterfaceLibrariesGenerator } from "./InterfaceLibrariesGenerator";
+export { default as Compiler, ILibrary } from "./Compiler";
+export { default as mapVariablePositions } from "./mapVariablePositions";
+export { default as Parser } from "./Parser";
--- pkgconfig/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as PkgConfigFileParser } from "./PkgConfigFileParser";
-export { default as resolveVariables } from "./resolveVariables";
-export { default as mapVariablePositions } from "./mapVariablePositions";
